a8f87e60a920176c9e7a68f966ae493b7c51964b
Derrick Fox <HomeFolder@Derricks-Mac-mini.local>
2014-10-28 13:04:14 -0400

    Initial Commit

---
+++ DoubleFStreamTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DoubleFStreamTest
+//
+//  Created by Derrick Fox on 10/28/14.
+//  Copyright (c) 2014 Derrick Fox. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
