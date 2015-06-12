e51e49ae3615ad16889a3553eac646bc43e53290
derrickfox <fox.derrick@gmail.com>
2015-06-12 14:32:57 -0400

    Initial Commit

--- DoubleFStreamTest/main.cpp
+++ DoubleFStreamTest/main.cpp
@@ -1,15 +1,33 @@
-//
-//  main.cpp
-//  DoubleFStreamTest
-//
-//  Created by Derrick Fox on 10/28/14.
-//  Copyright (c) 2014 Derrick Fox. All rights reserved.
-//
-
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    string fileName = "StartFile.txt";
+    ofstream outFile;
+    ifstream inFile(fileName.c_str());
+    string word1, word2, word3;
+    //inFile.open("StartFile.txt");
+    outFile.open("EndFile.txt");
+    
+    
+    // This copies one file to another AND prints out what was copied to the console.
+    if (inFile.is_open()){
+        //while (getline(inFile, word1, word2))
+        {
+            inFile >> word1 >> word2 >> word3;
+            cout << word1 << " " << word2 << " " << word3 << "\n";
+            outFile << word1 << " " << word2 << " " << word3 << "\n";
+        }
+    }
+    /////////////////////////////////////////////////////////////////////////////////
+    
+    inFile.close();
+    
+    cout << "Run Completed.\n";
     return 0;
+    
 }
